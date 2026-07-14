9df19c83f60e294d8ac7a4921d2cb9cb6dc14475
御风 <eternallyyf@163.com>
2026-07-14 14:16:46 +0800

    feat: 添加 -Mirror 参数支持切换 GitHub/Gitee 源

--- install_dev_env.ps1
+++ install_dev_env.ps1
@@ -1,8 +1,8 @@
 #Requires -Version 5.1
 [CmdletBinding()]
 param(
-    [switch]$Force,
-    [switch]$DryRun
+    [ValidateSet("github", "gitee")]
+    [string]$Mirror = "github"
 )
 
 $ErrorActionPreference = "Stop"
@@ -15,8 +15,10 @@ $env:JAVA_DOWNLOAD_URL = "https://mirrors.huaweicloud.com/openjdk/21.0.2/openjdk
 $JAVA_INSTALL_PATH = "C:\ProgramData\Java\jdk-21"
 $MINICONDA_INSTALL_PATH = $env:CONDA_INSTALL_PATH, "C:\ProgramData\miniconda3" | Select-Object -First 1
 
-# Miniconda 远程脚本地址
-$MINICONDA_SCRIPT_URL = "https://raw.githubusercontent.com/ashj-yf/conda_install_script/master/install_miniconda.ps1"
+# Miniconda 远程脚本地址（根据镜像源选择）
+$GITHUB_MINICONDA_URL = "https://raw.githubusercontent.com/ashj-yf/conda_install_script/master/install_miniconda.ps1"
+$GITEE_MINICONDA_URL = "https://gitee.com/ashj-yf/conda_install_script/raw/master/install_miniconda.ps1"
+$MINICONDA_SCRIPT_URL = if ($Mirror -eq "gitee") { $GITEE_MINICONDA_URL } else { $GITHUB_MINICONDA_URL }
 
 # 临时目录
 $TEMP_DIR = $env:TEMP
